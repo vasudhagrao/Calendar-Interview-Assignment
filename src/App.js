b8a50d758dcdccfb6f4d803635dc227da4c135c0
Vasudha G Rao <vasurao1996@gmail.com>
2022-04-26 15:23:54 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React, { useState, useContext, useEffect } from "react";
+import "./App.css";
+import { getMonth } from "./util";
+import CalendarHeader from "./components/CalendarHeader";
+import Month from "./components/Month";
+import GlobalContext from "./context/GlobalContext";
+import EventModal from "./components/EventModal";
+function App() {
+  const [currentMonth, setCurrentMonth] = useState(getMonth());
+  const { monthIndex, showEventModal } = useContext(GlobalContext);
+
+  useEffect(() => {
+    setCurrentMonth(getMonth(monthIndex));
+  }, [monthIndex]);
+
+  return (
+    <React.Fragment>
+      {showEventModal && <EventModal />}
+
+      <div className="h-screen flex flex-col">
+        <CalendarHeader />
+        <div className="flex flex-1">
+          <Month month={currentMonth} />
+        </div>
+      </div>
+    </React.Fragment>
+  );
+}
+
+export default App;
